e41bad2fb9c971ee15d766049c2128579296cdea
Phoenix Mystique <67161169+PhoenixMystique@users.noreply.github.com>
2025-02-13 06:44:50 +0000

    FIX: removed high level exception handling to throw identfied errors, Feat: Implemented Custom delimiter handling

--- app/service/strCalculator.service.js
+++ app/service/strCalculator.service.js
@@ -2,25 +2,32 @@ class StringCalculator {
     
     // Made the method static so that we can call it without creating an instance of the class
    static  add(rawString) {
-        try{
             if (!rawString) {
                 return 0;
             }
             if (typeof rawString !== 'string') {
                return  "Invalid input type";
             }
-            const numbersArray = rawString.split(/[,\n]/);
-            let sum = 0;
-            for (let i = 0; i < numbersArray.length; i++) {
-              sum += parseInt(numbersArray[i], 10);
+            //Inital Delimiter without including custom
+            let delimiter = /[,\n]/;
+        
+            if (rawString.startsWith('//')) {
+              const delEndIndex = rawString.indexOf('\n');
+              const customDelimiter = rawString.substring(2, delEndIndex);
+              delimiter = new RegExp(`[${customDelimiter},\n]`);
+              rawString = rawString.substring(delEndIndex + 1);
             }
-            return sum;
-        }
-        catch(err){
-            return"An error occurred:", err;
-        }
 
-    }
+            const numbersArray = rawString.split(delimiter);
+            // if -0 is there the parse will convert it into 0.
+            const numbers = numbersArray.map(rawNumber => parseInt(rawNumber, 10));
+            const negativeNumbers = numbers.filter(item => item < 0);
+            
+            if (negativeNumbers.length > 0) {
+              throw new Error(`Negative numbers not allowed ${negativeNumbers.join(',')}`);
+            }
+            return numbers.reduce((sum, num) => sum + num, 0);
+        }
     
   }
 module.exports = StringCalculator;
--- app/test/strCalculator.test.js
+++ app/test/strCalculator.test.js
@@ -25,4 +25,17 @@ describe('StringCalculator', () => {
   test('add numbers with newlines and commas returns sum', () => {
     expect(StringCalculator.add('1\n2,3')).toBe(6);
   });
+  test('custom delimiter returns sum', () => {
+    expect(StringCalculator.add('//;\n1;2')).toBe(3);
+  });
+  test('add negative number will throw exception', () => {
+    expect(() => StringCalculator.add('-1')).toThrow('Negative numbers not allowed -1');
+  });
+
+  test('add with multiple negatives throws exception listing all', () => {
+    expect(() => StringCalculator.add('2,-0,-3,-5')).toThrow('Negative numbers not allowed -3,-5');
+  });
+  test('add negatives number with custom delimiter throws exception listing all', () => {
+    expect(() => StringCalculator.add('//;\n2;-0;-3;-5')).toThrow('Negative numbers not allowed -3,-5');
+  });
 });
